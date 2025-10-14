048405205de6625045ffffe50a3ba3d7665d82e5
Han <hkjbrian@gmail.com>
2025-10-14 20:57:34 +0900

    feat: ErrorCode 생성

--- src/main/java/com/community/global/exception/ErrorCode.java
+++ src/main/java/com/community/global/exception/ErrorCode.java
@@ -8,9 +8,14 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
+    // user Domain
     UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
     DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "이미 사용 중인 닉네임입니다."),
+
+    // file domain
+    INVALID_FILE(HttpStatus.BAD_REQUEST, "업로드할 파일이 유효하지 않습니다."),
+    FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 파일을 찾을 수 없습니다."),
     FILE_STORAGE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 저장에 실패했습니다.");
 
     private final HttpStatus status;
