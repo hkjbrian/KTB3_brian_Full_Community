4ad73e1ae8c15f544e6710dec9ca867d020e1be6
Han <hkjbrian@gmail.com>
2025-10-15 15:45:38 +0900

    feat: UserController 내 정보, 비밀번호 수정 API 추가

--- src/main/java/com/community/domain/user/controller/UserController.java
+++ src/main/java/com/community/domain/user/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.community.domain.user.controller;
 
-import com.community.domain.user.dto.response.SignInAvailableResponse;
-import com.community.domain.user.dto.response.UserResponse;
-import com.community.domain.auth.dto.AuthenticatedUser;
 import com.community.domain.auth.annotation.Auth;
 import com.community.domain.auth.annotation.AuthUser;
-import com.community.global.response.ApiResponse;
+import com.community.domain.auth.dto.AuthenticatedUser;
+import com.community.domain.user.dto.request.PasswordUpdateRequest;
 import com.community.domain.user.dto.request.SignInRequest;
+import com.community.domain.user.dto.request.UpdateRequest;
+import com.community.domain.user.dto.response.SignInAvailableResponse;
 import com.community.domain.user.dto.response.SignInResponse;
+import com.community.domain.user.dto.response.UserResponse;
 import com.community.domain.user.service.UserService;
+import com.community.global.response.ApiResponse;
 import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -33,7 +35,8 @@ public class UserController {
     }
 
     @GetMapping("/availability")
-    public ResponseEntity<ApiResponse<SignInAvailableResponse>> checkAvailableSignInInfo(@RequestParam @Nullable String email, @RequestParam @Nullable String nickname) {
+    public ResponseEntity<ApiResponse<SignInAvailableResponse>> checkAvailableSignInInfo(@RequestParam @Nullable String email,
+                                                                                         @RequestParam @Nullable String nickname) {
         SignInAvailableResponse res = userService.checkAvailableSignInInfo(email, nickname);
         return ResponseEntity
                 .ok()
@@ -47,5 +50,27 @@ public class UserController {
         return ResponseEntity
                 .ok()
                 .body(ApiResponse.success("사용자 정보 조회에 성공했습니다.", profile));
+    }
+
+    @Auth
+    @PatchMapping(value = "/me")
+    public ResponseEntity<ApiResponse<Void>> updateProfile(@AuthUser AuthenticatedUser authenticatedUser,
+                                                           @ModelAttribute @Valid UpdateRequest req) {
+        userService.updateProfile(authenticatedUser.userId(), req);
+
+        return ResponseEntity
+                .ok()
+                .body(ApiResponse.success("사용자 정보가 수정되었습니다.", null));
+    }
+
+    @Auth
+    @PatchMapping("/me/password")
+    public ResponseEntity<ApiResponse<Void>> updatePassword(@AuthUser AuthenticatedUser authenticatedUser,
+                                                            @RequestBody @Valid PasswordUpdateRequest req) {
+        userService.changePassword(authenticatedUser.userId(), req);
+
+        return ResponseEntity
+                .ok()
+                .body(ApiResponse.success("비밀번호가 수정되었습니다.", null));
     }
 }
