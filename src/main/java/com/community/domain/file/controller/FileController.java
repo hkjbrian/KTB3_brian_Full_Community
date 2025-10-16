dd844083dce4bab4066498499ce77e1bd63485dd
Han <hkjbrian@gmail.com>
2025-10-16 21:13:03 +0900

    feat: 파일 컨트롤러

---
+++ src/main/java/com/community/domain/file/controller/FileController.java
@@ -0,0 +1,44 @@
+package com.community.domain.file.controller;
+
+import com.community.domain.file.service.FileStorageService;
+import com.community.domain.file.service.dto.StoredFile;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/files")
+public class FileController {
+
+    private final FileStorageService fileStorageService;
+
+    @GetMapping("/{fileId}")
+    public ResponseEntity<Resource> download(@PathVariable String fileId) {
+        StoredFile file = fileStorageService.load("/files/" + fileId);
+        MediaType contentType = file.getContentType() != null
+                ? MediaType.parseMediaType(file.getContentType())
+                : MediaType.APPLICATION_OCTET_STREAM;
+
+        String filename = file.getOriginalFilename() != null ? file.getOriginalFilename() : (fileId + ".bin");
+
+        return ResponseEntity.ok()
+                .contentType(contentType)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                .body(new ByteArrayResource(file.getContent()));
+    }
+
+    @DeleteMapping("/{fileId}")
+    public ResponseEntity<Void> delete(@PathVariable String fileId) {
+        fileStorageService.delete("/files/" + fileId);
+        return ResponseEntity.noContent().build();
+    }
+}
