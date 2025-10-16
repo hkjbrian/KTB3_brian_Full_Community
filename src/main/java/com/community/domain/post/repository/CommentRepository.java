2368052fd2759e5835ce160ae7c4fa3eb151420f
Han <hkjbrian@gmail.com>
2025-10-16 16:29:41 +0900

    feat: 댓글 도메인 개발 - Comment 엔티티 생성 - CommentRepository 인터페이스와 InMemoryCommentRepository 구현체 생성

---
+++ src/main/java/com/community/domain/post/repository/CommentRepository.java
@@ -0,0 +1,14 @@
+package com.community.domain.post.repository;
+
+import com.community.domain.post.model.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository {
+
+    Long save(Comment comment);
+    void delete(Comment comment);
+    Optional<Comment> findById(Long commentId);
+    List<Comment> findByPostId(Long postId);
+}
