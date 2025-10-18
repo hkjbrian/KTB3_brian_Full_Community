4b4d36d049957199e6a6307f26f12feb6b772d47
Han <hkjbrian@gmail.com>
2025-10-18 21:58:34 +0900

    feat: SwaggerConfig 추가 swagger ui 설정 - @AuthUser, AuthenticatedUser 무시 설정 - JWT 를 통한 authorize 설정

---
+++ src/main/java/com/community/global/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.community.global.config;
+
+import com.community.domain.auth.annotation.AuthUser;
+import com.community.domain.auth.dto.AuthenticatedUser;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.utils.SpringDocUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    static {
+        SpringDocUtils.getConfig()
+                .addAnnotationsToIgnore(AuthenticatedUser.class, AuthUser.class);
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Community Test") // API의 제목
+                .description("커뮤니티 서버 API") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
+    }
+}
