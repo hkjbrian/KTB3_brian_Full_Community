afedf166e0303e506abc17fabc8f64f757999434
Han <hkjbrian@gmail.com>
2025-10-20 21:12:24 +0900

    feat: AuthController API swagger operation 작성

--- src/main/java/com/community/domain/auth/controller/AuthController.java
+++ src/main/java/com/community/domain/auth/controller/AuthController.java
@@ -7,6 +7,8 @@ import com.community.global.exception.ErrorCode;
 import com.community.domain.auth.dto.response.LoginResponse;
 import com.community.domain.auth.dto.request.LoginRequest;
 import com.community.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Auth", description = "인증/인가 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -25,6 +28,7 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "로그인", description = "아이디와 비밀번호를 통해 로그인을 요청합니다.")
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<LoginResponse>> login(@RequestBody @Valid LoginRequest req) {
         LoginResult result = authService.login(req);
@@ -41,6 +45,7 @@ public class AuthController {
                 .body(ApiResponse.success("로그인에 성공하였습니다.", result.tokenResponse()));
     }
 
+    @Operation(summary = "토큰 재발급", description = "accessToken 이 만료되었을 경우, 쿠키의 refreshToken 을 통해 토큰을 재발급합니다.")
     @PostMapping("/refresh")
     public ResponseEntity<ApiResponse<LoginResponse>> refresh(
             @CookieValue(value = "refreshToken") String refreshToken
