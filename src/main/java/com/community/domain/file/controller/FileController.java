173cf0eab74151e70fff01ea504a73336fdc961e
Han <hkjbrian@gmail.com>
2025-10-20 21:09:39 +0900

    feat: FileController API swagger operation 작성

--- src/main/java/com/community/domain/file/controller/FileController.java
+++ src/main/java/com/community/domain/file/controller/FileController.java
@@ -2,6 +2,8 @@ package com.community.domain.file.controller;
 
 import com.community.domain.file.service.FileStorageService;
 import com.community.domain.file.service.dto.StoredFile;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "File", description = "파일 관리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/files")
@@ -21,6 +24,7 @@ public class FileController {
 
     private final FileStorageService fileStorageService;
 
+    @Operation(summary = "파일 다운로드", description = "fileId 를 기준으로 해당 파일을 다운로드합니다.")
     @GetMapping("/{fileId}")
     public ResponseEntity<Resource> download(@PathVariable String fileId) {
         StoredFile file = fileStorageService.load(fileId);
@@ -36,6 +40,7 @@ public class FileController {
                 .body(new ByteArrayResource(file.getContent()));
     }
 
+    @Operation(summary = "파일 삭제", description = "fileId 를 기준으로 해당 파일을 삭제합니다.")
     @DeleteMapping("/{fileId}")
     public ResponseEntity<Void> delete(@PathVariable String fileId) {
         fileStorageService.delete("/files/" + fileId);
