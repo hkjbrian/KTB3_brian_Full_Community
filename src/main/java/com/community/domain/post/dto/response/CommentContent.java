3cf0f898c723f0da143c0efd80bd8e562183ff2e
Han <hkjbrian@gmail.com>
2025-10-20 20:46:02 +0900

    feat: CommentController 관련 DTO들 swagger schema 작성

--- src/main/java/com/community/domain/post/dto/response/CommentContent.java
+++ src/main/java/com/community/domain/post/dto/response/CommentContent.java
@@ -1,10 +1,12 @@
 package com.community.domain.post.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "댓글 내용 응답 DTO")
 @Getter
 @AllArgsConstructor
 public class CommentContent {
--- src/main/java/com/community/domain/post/dto/response/CommentIdResponse.java
+++ src/main/java/com/community/domain/post/dto/response/CommentIdResponse.java
@@ -1,8 +1,10 @@
 package com.community.domain.post.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Schema(description = "댓글 Id 만 반환하는 응답 DTO")
 @Getter
 @AllArgsConstructor
 public class CommentIdResponse {
