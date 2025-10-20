c38acaf9ccd9c84172bbd7b5aa53b1a09f435117
Han <hkjbrian@gmail.com>
2025-10-20 21:08:03 +0900

    feat: CommentController API swagger operation 작성

--- src/main/java/com/community/domain/post/controller/CommentController.java
+++ src/main/java/com/community/domain/post/controller/CommentController.java
@@ -8,6 +8,8 @@ import com.community.domain.post.dto.response.CommentIdResponse;
 import com.community.domain.post.dto.response.CommentListResponse;
 import com.community.domain.post.service.CommentService;
 import com.community.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
+@Tag(name = "Comment", description = "댓글 관리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts/{postId}/comments")
@@ -29,6 +32,7 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    @Operation(summary = "댓글 리스트 조회", description = "페이징을 기반으로 댓글 리스트를 조회합니다.")
     @GetMapping
     public ResponseEntity<ApiResponse<CommentListResponse>> getComments(@PathVariable Long postId,
                                                                         @RequestParam(defaultValue = "0") int page,
@@ -40,6 +44,7 @@ public class CommentController {
                 .body(ApiResponse.success("게시글 댓글 조회에 성공했습니다.", res));
     }
 
+    @Operation(summary = "댓글 추가", description = "특정 게시글에 댓글을 작성합니다.")
     @Auth
     @PostMapping
     public ResponseEntity<ApiResponse<CommentIdResponse>> createComment(@PathVariable Long postId,
@@ -52,6 +57,7 @@ public class CommentController {
                 .body(ApiResponse.success("게시글 댓글 등록에 성공했습니다.", res));
     }
 
+    @Operation(summary = "댓글 수정", description = "특정 게시글에 달려있는 특정 댓글을 수정합니다.")
     @Auth
     @PutMapping("/{commentId}")
     public ResponseEntity<ApiResponse<CommentIdResponse>> updateComment(@PathVariable Long postId,
@@ -65,6 +71,7 @@ public class CommentController {
                 .body(ApiResponse.success("게시글 댓글 수정에 성공했습니다.", response));
     }
 
+    @Operation(summary = "댓글 삭제", description = "특정 게시글에 달려있는 특정 댓글을 삭제합니다.")
     @Auth
     @DeleteMapping("/{commentId}")
     public ResponseEntity<ApiResponse<Void>> deleteComment(@PathVariable Long postId,
