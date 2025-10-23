7ab2bed5159e4f42ed32d722d2d6e74def054c84
Han <hkjbrian@gmail.com>
2025-10-23 17:14:59 +0900

    fix: File 을 반환하는 경우 filename 이 한국어인 경우 헤더가 삭제되어버리는 문제 해결

--- src/main/java/com/community/domain/file/controller/FileController.java
+++ src/main/java/com/community/domain/file/controller/FileController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
 
 @Tag(name = "File", description = "파일 관리 API")
 @RestController
@@ -33,10 +36,14 @@ public class FileController {
                 : MediaType.APPLICATION_OCTET_STREAM;
 
         String filename = file.getOriginalFilename() != null ? file.getOriginalFilename() : (fileId + ".bin");
+        ContentDisposition cd = ContentDisposition.attachment()
+                .filename(filename, StandardCharsets.UTF_8) // <= 핵심: UTF-8 filename*
+                .build();
 
         return ResponseEntity.ok()
                 .contentType(contentType)
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                .header(HttpHeaders.CONTENT_DISPOSITION, cd.toString())
+                .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.CONTENT_DISPOSITION)
                 .body(new ByteArrayResource(file.getContent()));
     }
 
