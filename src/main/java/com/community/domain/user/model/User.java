7b00f41c03c5bd5e1e2030ec3b847543e5911869
Han <hkjbrian@gmail.com>
2025-11-01 21:01:12 +0900

    feat: User 엔티티 JPA 애노테이션 적용 - BaseTimeEntity 상속하도록 수정 - @Entity, @Id를 통해서 엔티티임을 명시 - 각 필드에 대해서 데이터베이스와 제약조건 일치하도록 애노테이션 추가

--- src/main/java/com/community/domain/user/model/User.java
+++ src/main/java/com/community/domain/user/model/User.java
@@ -1,14 +1,42 @@
 package com.community.domain.user.model;
 
+import com.community.domain.common.model.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Generated;
 
+@Entity
 @Getter
-public class User {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User extends BaseTimeEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
+
+    @Email
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true)
     private String email;
+
+    @NotBlank
+    @Size(max = 255)
     private String password;
+
+    @NotBlank
+    @Size(max = 10)
     private String nickname;
+
+    @Generated()
+    @Size(max = 255)
     private String imageUrl;
 
     public User(String email, String password, String nickname, String imageUrl) {
