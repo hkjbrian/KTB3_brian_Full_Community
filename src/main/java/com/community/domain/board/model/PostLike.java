0d08717cf15c672d80effc498d70423e6edeb644
Han <hkjbrian@gmail.com>
2025-11-02 17:58:16 +0900

    feat: PostLike 엔티티 애노테이션 적용 - 복합키 사용 대신 대리키 필드를 추가하여 간단하게 조회할 수 있도록 수정

--- src/main/java/com/community/domain/board/model/PostLike.java
+++ src/main/java/com/community/domain/board/model/PostLike.java
@@ -1,16 +1,33 @@
 package com.community.domain.board.model;
 
 import com.community.domain.common.model.BaseTimeEntity;
+import com.community.domain.user.model.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "post_likes")
 public class PostLike extends BaseTimeEntity {
 
-    private final Long postId;
-    private final Long userId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_like_id")
+    private Long id;
 
-    public PostLike(Long postId, Long userId) {
-        this.postId = postId;
-        this.userId = userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="user_id")
+    private User user;
+
+    public PostLike(Post post, User user) {
+        this.post = post;
+        this.user = user;
     }
 }
