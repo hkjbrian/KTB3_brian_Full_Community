dc0049736b87a8b2e02906e839ff6cce66dc1a3e
Han <hkjbrian@gmail.com>
2025-11-02 17:57:29 +0900

    feat: Comment 엔티티 애노테이션 적용 - @Entity, @Id 를 통해서 엔티티임을 명시 - 각 필드에 대해서 데이터베이스와 제약조건 일치하도록 애노테이션 추가

--- src/main/java/com/community/domain/board/model/Comment.java
+++ src/main/java/com/community/domain/board/model/Comment.java
@@ -1,19 +1,39 @@
 package com.community.domain.board.model;
 
 import com.community.domain.common.model.BaseTimeEntity;
+import com.community.domain.user.model.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comments")
 public class Comment extends BaseTimeEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
     private Long id;
-    private final Long postId;
-    private final Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Lob
+    @NotBlank
     private String body;
 
-    public Comment(Long postId, Long userId, String body) {
-        this.postId = postId;
-        this.userId = userId;
+    public Comment(Post post, User user, String body) {
+        this.post = post;
+        this.user = user;
         this.body = body;
     }
 
