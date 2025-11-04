3e9be4b2919aeed270f2d94097992877549d2f53
Han <hkjbrian@gmail.com>
2025-11-04 23:29:57 +0900

    feat: PostViewEventRepository 생성 - 로그 저장 - 최신순으로 저장된 로그 불러오기 - 처리된 로그에 대한 상태 수정하기

---
+++ src/main/java/com/community/domain/board/repository/JpaPostViewEventRepository.java
@@ -0,0 +1,38 @@
+package com.community.domain.board.repository;
+
+import com.community.domain.board.model.PostViewEvent;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
+
+@Repository
+public class JpaPostViewEventRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public PostViewEvent save(PostViewEvent postViewEvent) {
+        em.persist(postViewEvent);
+        return postViewEvent;
+    }
+
+    public List<PostViewEvent> findByLimitOrderByIdAsc(int limit) {
+        return em.createQuery("select e from PostViewEvent e " +
+                        "where e.status = :eventType " +
+                        "order by e.id asc", PostViewEvent.class)
+                .setParameter("eventType", PostViewEvent.Status.PENDING)
+                .setFirstResult(0)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public void updateEventType(Set<Long> ids) {
+        em.createQuery("update PostViewEvent e set e.status = :eventType where e.id in :ids")
+                .setParameter("eventType", PostViewEvent.Status.DONE)
+                .setParameter("ids", ids)
+                .executeUpdate();
+    }
+}
